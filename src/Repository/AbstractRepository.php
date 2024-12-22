0df7c3cc6ab961cba23a02322c77f2ee640065e3
sweet0dream <sweet0dream@proton.me>
2024-12-23 01:09:36 +0400

    fixed throws for abstract repository

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -16,29 +16,32 @@ abstract class AbstractRepository
         $this->connect = (new Db())->connect();
     }
 
+    /**
+     * @throws Exception
+     */
     protected function get(
         string $model,
         ?array $where = null,
         ?array $orderBy = null
     ): array|string
     {
-        try {
-            if (!is_null($where)) {
-                foreach ($where as $key => $value) {
-                    $this->connect->where($key, $value);
-                }
+        if (!is_null($where)) {
+            foreach ($where as $key => $value) {
+                $this->connect->where($key, $value);
             }
-            if (!is_null($orderBy)) {
-                foreach ($orderBy as $key => $value) {
-                    $this->connect->orderBy($key, $value);
-                }
-            }
-            return $this->connect->get($model);
-        } catch (Exception $e) {
-            die($e->getMessage());
         }
+        if (!is_null($orderBy)) {
+            foreach ($orderBy as $key => $value) {
+                $this->connect->orderBy($key, $value);
+            }
+        }
+
+        return $this->connect->get($model);
     }
 
+    /**
+     * @throws Exception
+     */
     public function findBy(array $criteria, array $orderBy = []): array
     {
         foreach ($criteria as $key => $value) {
@@ -52,11 +55,17 @@ abstract class AbstractRepository
         return $this->get($this->modelClass);
     }
 
+    /**
+     * @throws Exception
+     */
     public function findOne(int $id): ?array
     {
         return $this->get($this->modelClass, ['id' => $id])[0] ?? null;
     }
 
+    /**
+     * @throws Exception
+     */
     public function findOneBy(array $criteria): ?array
     {
         foreach ($criteria as $key => $value) {
@@ -66,6 +75,9 @@ abstract class AbstractRepository
         return $this->get($this->modelClass)[0] ?? null;
     }
 
+    /**
+     * @throws Exception
+     */
     public function updateById(int $id, array $data): array
     {
         $this->connect->where('id', $id)->update($this->modelClass, $data);
@@ -73,6 +85,9 @@ abstract class AbstractRepository
         return $this->findOneBy(['id' => $id]);
     }
 
+    /**
+     * @throws Exception
+     */
     public function insert(array $data): array
     {
         $this->connect->insert($this->modelClass, $data);
@@ -80,6 +95,9 @@ abstract class AbstractRepository
         return $this->findOneBy(['id' => $this->connect->getInsertId()]);
     }
 
+    /**
+     * @throws Exception
+     */
     public function deleteById(int $id): bool
     {
         return $this->connect->where('id', $id)->delete($this->modelClass);
